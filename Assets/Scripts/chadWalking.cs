6e48e8b9f35107598be4f1f6ce195e0b655a202e
Baugrems <copenhagenmhs@gmail.com>
2017-11-11 02:30:23 -0700

    Initial commit

---
+++ Assets/Scripts/chadWalking.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chadWalking : MonoBehaviour {
+
+	public float xmin;
+	public float xmax;
+	public float padding = 1f;
+	public GameObject chatScript;
+	public LevelManager levelManager;
+	public AudioClip clip;
+
+	private int spriteSteps;
+
+	public Sprite[] chadSprite;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource.PlayClipAtPoint (clip, this.transform.position);
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+		spriteSteps = 0;
+		levelManager = FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+			transform.position = new Vector3 (Mathf.Clamp (transform.position.x + .1f, xmin, xmax), transform.position.y,transform.position.z);
+			loadNextSprite();
+		} else if (Input.GetKey(KeyCode.A)) {
+			transform.position = new Vector3 (Mathf.Clamp (transform.position.x - .1f, xmin, xmax),transform.position.y,transform.position.z);
+			loadNextSprite();
+		} else if (Input.GetKeyDown(KeyCode.Space)) {
+			AudioSource.PlayClipAtPoint (clip, this.transform.position);
+			GetComponent<SpriteRenderer> ().sprite = chadSprite[3];
+			if (chatScript.GetComponent<chatHandler> ().chatted == true) {
+				chatScript.GetComponent<chatHandler> ().chatty.text = "You will pay for this!\n\n" +
+				"Come get some, bro!" +
+				"\n\nPress Spacebar to Continue";
+			}
+		}
+	}
+
+	void loadNextSprite (){
+		if (spriteSteps == 3) {
+			spriteSteps++;
+		}
+		if (spriteSteps < chadSprite.Length) {
+			GetComponent<SpriteRenderer> ().sprite = chadSprite [spriteSteps];
+			spriteSteps++;
+		} else {
+			spriteSteps = 0;
+			loadNextSprite ();
+		}
+	}
+}
---
+++ Assets/Scripts/chatHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chatHandler : MonoBehaviour {
+	public Text chatty;
+	public Image chatbox;
+	public bool chatted = false;
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		chatbox.transform.position = new Vector3 (400f, 400f, 0f);
+		chatty.text = "Aha! Looks like you are missing your prized belt, Chad.\n\n" +
+			"Press Space to Continue";
+		chatted = true;
+
+	}
+		
+}
